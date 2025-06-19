fd27782397f032451e3972bfc834dceedb79d363
RV <a@b.com>
2025-06-19 21:55:55 +0100

    first commit

---
+++ L1_shared.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+#include <pthread.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS 5
+#define ITERATIONS 100000000
+
+typedef struct {
+  long value; // Potential false sharing if structs are close in memory
+} SharedData;
+
+SharedData data[NUM_THREADS];
+
+void *worker(void *arg) {
+  int id = *(int *)arg;
+  for (long i = 0; i < ITERATIONS; ++i) {
+    data[id].value++;
+  }
+  return NULL;
+}
+
+int main() {
+  pthread_t threads[NUM_THREADS];
+  int ids[NUM_THREADS];
+
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    ids[i] = i;
+    pthread_create(&threads[i], NULL, worker, &ids[i]);
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(i, &cpuset); // wrap if threads > cores
+    pthread_setaffinity_np(threads[i], sizeof(cpu_set_t), &cpuset);
+  }
+
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    printf("Thread %d: %ld\n", i, data[i].value);
+  }
+
+  return 0;
+}
